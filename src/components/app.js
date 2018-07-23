22bc8062be9e569a45de68d92499a8d1a52b46e9
DevCamp <DevCamp@708S-LMCA-111.canyonsdistrict.pvt>
2018-07-23 11:52:01 -0600

    did stuff

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
+
 import Picker from "./picker";
 import Button from "./button";
 import Clock from "./clock";
 import ChangeDate from "./changeDate";
 import LargeText from "./largeText";
+
 import moment from "moment";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
 
-this.timer = 0;
+    this.timer = 0;
 
-this.state = {
+    this.state = {
       active: false,
       startDate: moment(),
       timeRemaining: {
@@ -23,7 +25,8 @@ this.state = {
       },
       age: 0
     };
-   this.handleGenerate = this.handleGenerate.bind(this);
+
+    this.handleGenerate = this.handleGenerate.bind(this);
   }
 
   handleChange = function(date) {
@@ -132,14 +135,16 @@ this.state = {
     return (
       <div className="grid">
         <h1 className="grid__title">Birthday Countdown</h1>
-<div className="grid__skew-dark-two" />
-<div className="grid__skew-dark-three" />
-<div className="grid__skew-light-one" />
-<div className="grid__skew-light-two" />
-<div className="grid__skew-light-three-box" />
+
+        <div className="grid__skew-dark-two" />
+        <div className="grid__skew-dark-three" />
+
+        <div className="grid__skew-light-one" />
+        <div className="grid__skew-light-two" />
+        <div className="grid__skew-light-three-box" />
 
         {this.renderItems()}
- </div>
+      </div>
     );
   }
 }
